325ecdf50d0cf4371a21c5b33541c7b27ad52603
sydimran <imran.syd1910@gmail.com>
2024-08-16 15:42:14 +0530

    Database Connectivity

--- FirstDangoProject/settings.py
+++ FirstDangoProject/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'FirstDangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'imrandb',
+        'USER': 'root',
+        'PASSWORD': 'Tiger',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',                 
     }
 }
+
 
 
 # Password validation
