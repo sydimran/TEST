69c9e8b12ce7b38100c773e31ea3443a10f75605
sydimran <imran.syd1910@gmail.com>
2024-08-19 10:25:28 +0530

    Learn Models amd Migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class laptop(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+    Price = models.DecimalField(max_digits=64, decimal_places=2)
+    Purchase_date = models.DateTimeField()
